709f4b6ff0ff8de9015769d275df2b2912a96995
Matt Smith <v-matsm@microsoft.com>
2020-04-24 17:29:08 -0500

    Cleaned up - made app accept arguments.

--- TweetPurge/Program.cs
+++ TweetPurge/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.CommandLineUtils;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Tweetinvi;
@@ -6,27 +7,67 @@ using Tweetinvi.Models;
 
 namespace TweetPurge
 {
+    //// Get your Twitter Consumer Key and Secret here:
+    //// https://dev.twitter.com
+    //// Setup a new app, and the info should be within said app.
     class Program
     {
         static async Task Main(string[] args)
         {
-            // Get your Twitter Consumer Key and Secret here:
-            // https://dev.twitter.com
-            // Setup a new app, and the info should be within said app.
-            var appCredentials = new TwitterCredentials("CONSUMERKEY","CONSUMERSECRET");
+            var app = new CommandLineApplication();
+            app.Name = "TweetPurge";
+            app.HelpOption("-?|-h|--help");
+
+            app.OnExecute(() =>
+            {
+                Console.WriteLine("Please include your Consumer Key and Consumer Secret.");
+                Console.WriteLine(@"Example: .\TweetPurge purge -ck MYKEY -cs MYSECRET");
+                return 0;
+            });
+
+            app.Command("purge", (command) =>
+            {
+                command.Description = "Purge your entire Twitter history.";
+                command.HelpOption("-?|-h|--help");
+
+                var consumerKeyOption = command.Option("-ck|--ConsumerKey", "Consumer Key for Twitter Application.", CommandOptionType.SingleValue);
+                var consumerSecretOption = command.Option("-cs|--ConsumerSecret", "Consumer Secret for Twitter Application.", CommandOptionType.SingleValue);
+
+                command.OnExecute(async () =>
+                {
+                    var consumerKey = consumerKeyOption.Values;
+                    var consumerSecret = consumerSecretOption.Values;
+
+                    if(consumerKey.Count == 0 || consumerSecret.Count == 0)
+                    {
+                        Console.WriteLine("Invalid Consumer Key or Consumer Secret provided.");
+                        return 0;
+                    }
+
+                    await ProcessTwitterAccount(consumerKey.FirstOrDefault(), consumerSecret.FirstOrDefault());
+
+                    Console.WriteLine("\r\n\r\nSuccessfully purged your Twitter timeline. Enjoy!")
+                    return 1;
+                });
+            });
+
+            app.Execute(args);
+        }
+
+        private static async Task ProcessTwitterAccount(string consumerKey, string consumerSecret)
+        {
+            var appCredentials = new TwitterCredentials(consumerKey, consumerSecret);
             var authenticationContext = AuthFlow.InitAuthentication(appCredentials);
-            
+
             Console.WriteLine("Copy the URL below. When it loads, and you are logged in, allow access to the application. Copy and paste the code below and hit enter.");
             Console.WriteLine($"\r\n{authenticationContext.AuthorizationURL}\r\n");
             Console.Write("Enter Code (and hit enter): ");
-            
+
             var pinCode = Console.ReadLine();
-            
+
             var authenticatedUser = GetAuthenticatedUser(pinCode, authenticationContext);
 
             await ProcessTweets(authenticatedUser);
-
-            Console.WriteLine("All set.");
         }
 
         private static IAuthenticatedUser GetAuthenticatedUser(string pinCode, IAuthenticationContext authenticationContext)
